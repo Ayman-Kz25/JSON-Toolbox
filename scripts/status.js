6ae34da82051f823d582623be3b9c3d3c856f088
Ayman-Kz25 <kamalakz786@gmail.com>
2026-08-08 18:28:02 +0500

    update status module

--- scripts/status.js
+++ scripts/status.js
@@ -3,22 +3,17 @@ import {
   statusText,
 } from "./dom.js";
 
-
 export function initStatus(
   message = "Ready",
   type = "default"
 ) {
-
   if (statusText) {
     statusText.textContent = message;
   }
 
-
   if (!jsonStatus) return;
 
-
   jsonStatus.className = "status-indicator";
-
 
   if (type !== "default") {
     jsonStatus.classList.add(type);
